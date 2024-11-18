deb15b77a424806b2206f4c5b2f61285915a0d30
Sascha Frank <sascha.frank.88@gmx.de>
2024-11-18 14:41:55 +0100

    edited filepath for xstartup

--- 03_setup-vncserver_postinstall.sh
+++ 03_setup-vncserver_postinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DESTINATION="/home/sascha/.vnc/xstartup"
-URL="https://raw.githubusercontent.com/Izzy3110/cloudserver-install/refs/heads/main/xstartup"
+URL="https://raw.githubusercontent.com/Izzy3110/cloudserver-install/refs/heads/main/vncserver/xstartup"
 
 if wget -O "$DESTINATION" "$URL"; then
     echo "Download successful! File saved to $DESTINATION."
