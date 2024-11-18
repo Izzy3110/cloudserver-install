54dc2a72ca9fd2aa35ed27b513bfdd8563867885
Sascha Frank <sascha.frank.88@gmx.de>
2024-11-18 14:31:56 +0100

    edited 01_apt

--- 01_apt.sh
+++ 01_apt.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-apt update && apt-get upgrade -y 
-apt install xfce4 xfce4-goodies firefox-esr autocutsel tightvncserver dbus-x11 xfonts-base -y
+
+apt-get update && apt-get upgrade -y 
+apt-get install git xfce4 xfce4-goodies firefox-esr autocutsel tightvncserver dbus-x11 xfonts-base -y
+
