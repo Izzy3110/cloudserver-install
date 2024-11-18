f36690783ab66b77d2b29185c5c3e5ad7cedb29d
Sascha Frank <sascha.frank.88@gmx.de>
2024-11-18 19:41:03 +0100

    Update 03_setup-vncserver_postinstall.sh

--- 03_setup-vncserver_postinstall.sh
+++ 03_setup-vncserver_postinstall.sh
@@ -3,7 +3,7 @@
 DESTINATION="/root/.vnc/xstartup"
 URL="https://raw.githubusercontent.com/Izzy3110/cloudserver-install/refs/heads/main/vncserver/xstartup"
 
-if wget -O "$DESTINATION" "$URL"; then
+if wget -q -O "$DESTINATION" "$URL"; then
     echo "Download successful! File saved to $DESTINATION."
 else
     echo "Download failed!" >&2
