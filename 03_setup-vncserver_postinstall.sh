e6834e18d7a6932480cb51b14456e0ed1cc98495
Sascha Frank <sascha.frank.88@gmx.de>
2024-12-20 22:58:50 +0100

    Update 03_setup-vncserver_postinstall.sh

--- 03_setup-vncserver_postinstall.sh
+++ 03_setup-vncserver_postinstall.sh
@@ -10,6 +10,26 @@ else
     exit 1
 fi
 
+mkdir -p $HOME/.local/share/applications
+DESKTOP_FILE_PATH="$HOME/.local/share/applications/firefox-esr.desktop"
+
+echo "Creating Firefox desktop launcher..."
+cat <<EOF > "$DESKTOP_FILE_PATH"
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Firefox ESR Web Browser
+Comment=Browse the web
+Exec=/usr/bin/firefox %u
+Icon=/usr/lib/firefox-esr/browser/chrome/icons/default/default128.png
+Terminal=false
+Categories=Network;WebBrowser;
+MimeType=text/html;text/xml;application/xhtml+xml;application/xml;x-scheme-handler/http;x-scheme-handler/https;x-scheme-handler/ftp;x-scheme-handler/chrome;application/x-xpinstall;
+StartupNotify=true
+EOF
+
+chmod +x "$DESKTOP_FILE_PATH"
+
 chmod +x $DESTINATION
 vncserver -kill :1
 vncserver -geometry 1920x1500
